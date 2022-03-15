b7cfa8df46c5391c0a5732840efdbed17eb19641
shilei <shilei@wuxicloud.com>
2022-03-15 16:02:56 +0800

    tiny-videos page

--- lib/view/sub_pages/tiny_videos.dart
+++ lib/view/sub_pages/tiny_videos.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TinyVideos extends StatefulWidget {
@@ -10,19 +11,33 @@ class _TinyVideos extends State<TinyVideos> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (BuildContext context, int index) {
-        return Column(
-          children: [
-            FadeInImage.assetNetwork(
-              image: 'assets/images/common/lazy-3.png',
-              placeholder: 'assets/images/common/lazy-3.png',
-              fit: BoxFit.cover,
-            )
-          ],
-        );
-      },
+    return GridView(
+      gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, //横轴三个子widget
+          childAspectRatio: 0.6 //宽高比为1时，子widget
+      ),
+      children: ListBox(),
     );
+    // return ListView.builder(
+    //   itemCount: 10,
+    //   itemBuilder: (BuildContext context, int index) {
+    //     return Column(
+
+    //     );
+    //   },
+    // );
   }
+
+  List<Widget> ListBox() => List.generate(20, (index) {
+    return Container(
+      height: 100,
+      color: Colors.amber,
+      margin: EdgeInsets.all(10),
+      child:  FadeInImage.assetNetwork(
+        image: 'assets/images/common/lazy-3.png',
+        placeholder: 'assets/images/common/lazy-3.png',
+        fit: BoxFit.cover,
+      ),
+    );
+  });
 }
