530933fb4a037007f6ff2384ed4046e71198aee7
shilei <shilei@wuxicloud.com>
2022-03-22 12:57:43 +0800

    comment part

---
+++ lib/components/comment_part.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class CommentPart extends StatelessWidget {
+
+  const CommentPart({Key? key, }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('comment part'),
+    );
+  }
+
+}
